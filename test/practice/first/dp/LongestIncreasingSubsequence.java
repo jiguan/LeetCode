54b5d12a7da382a6eb72db023b6becde360fc801
Jianqing Guan <gjqvictory888@gmail.com>
2016-04-18 20:26:50 -0400

    161 / 325

--- test/practice/first/dp/LongestIncreasingSubsequence.java
+++ test/practice/first/dp/LongestIncreasingSubsequence.java
@@ -6,24 +6,22 @@ import org.junit.Test;
 
 public class LongestIncreasingSubsequence {
 	public int lengthOfLIS(int[] nums) {
-		int n = nums.length;
-		if(n==0) return 0;
-		int[] arr = new int[n];
-		for(int i=0;i<n;i++) {
-			arr[i] = 1;
-		}
-		for(int i=1;i<n;i++) {
-			for(int j=0;j<i;j++) {
-				if(nums[i]>nums[j]) {
-					arr[i] = Math.max(arr[i], arr[j] + 1);
-				}
+		int len = nums.length;
+		int[] arr = new int[len];
+		int max = 0;
+		for(int i=0;i<len;i++) arr[i] = Integer.MAX_VALUE;
+		for(int i=0;i<len;i++) {
+			int element = nums[i];
+			int low = 0, high = i;
+			while(low<=high) {
+				int middle = (high-low)/2 + low;
+				if(arr[middle]>element) high = middle-1;
+				else low = middle+1;
 			}
+			arr[low] = element; 
+			max = Math.max(max, low+1);
 		}
-		int result = 0;
-		for(int i=0;i<n;i++) {
-			result = Math.max(result, arr[i]);
-		}
-		return result;
+		return max;
 	}
 	
 	@Test
@@ -32,3 +30,24 @@ public class LongestIncreasingSubsequence {
 		assertEquals(4, lengthOfLIS(nums));
 	}
 }
+
+
+/*	int n = nums.length;
+if(n==0) return 0;
+int[] arr = new int[n];
+for(int i=0;i<n;i++) {
+	arr[i] = 1;
+}
+for(int i=1;i<n;i++) {
+	for(int j=0;j<i;j++) {
+		if(nums[i]>nums[j]) {
+			arr[i] = Math.max(arr[i], arr[j] + 1);
+		}
+	}
+}
+int result = 0;
+for(int i=0;i<n;i++) {
+	result = Math.max(result, arr[i]);
+}
+return result;
+*/
