81fadeffd3227c76874347c88eb39fb704e608f2
Jianqing Guan <gjqvictory888@gmail.com>
2016-05-11 00:06:46 -0400

    199 / 331

---
+++ test/practice/first/array/CombinationSumIII.java
@@ -0,0 +1,59 @@
+package practice.first.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import practice.first.util.PrettyPrint;
+
+public class CombinationSumIII {
+	public List<List<Integer>> combinationSum3(int k, int n) {
+		List<List<Integer>> result = new ArrayList<>();
+		find(1, n, k, 0, new ArrayList<Integer>(), result);
+		return result;
+	}
+	
+	private void find(int start, int n, int k, int add, List<Integer> current, List<List<Integer>> result ) {
+		if(add==n) {
+			if(current.size()==k) {
+				result.add(new ArrayList<>(current));
+			}
+			return;
+		}
+		if(add>n||current.size()>=k) {
+			return;
+		}
+		for(int i=start;i<10;i++) {
+			current.add(i);
+			find(i+1, n, k, add+i, current, result);
+			current.remove(current.size()-1);
+		}
+	}
+	
+	@Test
+	public void test0() {
+		int k = 3, n = 9;
+		List<List<Integer>> result = combinationSum3(k, n);
+		for(List<Integer> list : result) {
+			PrettyPrint.print(list);
+		}
+	}
+	
+	@Test
+	public void test1() {
+		int k = 2, n = 18;
+		List<List<Integer>> result = combinationSum3(k, n);
+		for(List<Integer> list : result) {
+			PrettyPrint.print(list);
+		}
+	}
+	
+	@Test
+	public void test2() {
+		int i = -8;
+		int j = i>>>3;
+		System.out.println(j);
+	}
+}
--- test/practice/first/array/PermutationsII.java
+++ test/practice/first/array/PermutationsII.java
@@ -1,7 +1,49 @@
 package practice.first.array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+import practice.first.util.PrettyPrint;
+
 public class PermutationsII {
-	 public List<List<Integer>> permuteUnique(int[] nums) {
-	        
-	    }
+	public List<List<Integer>> permuteUnique(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		if(nums==null||nums.length==0) return result;
+		Arrays.sort(nums);
+		find(nums,new boolean[nums.length], new ArrayList<>(), result);
+		return result;
+	}
+	
+	private void find(int[] nums, boolean[] visited, List<Integer> current, List<List<Integer>> result) {
+		if(current.size()==nums.length) {
+			result.add(new ArrayList<Integer>(current));
+			return;
+		}
+		for(int i=0;i<nums.length;i++) {
+			if(visited[i]==false) {
+				if(i>0&&nums[i]==nums[i-1]&&!visited[i-1]) {
+					continue;
+				}
+				current.add(nums[i]);
+				visited[i] = true;
+				find(nums, visited, current,result);
+				current.remove(current.size()-1);
+				visited[i] = false;
+			}
+		}
+		
+	}
+	
+	@Test
+	public void test0() {
+		int[] nums = new int[]{1,1,2};
+		for(List<Integer> list: permuteUnique(nums)) {
+			PrettyPrint.print(list);
+		}
+	}
 }
