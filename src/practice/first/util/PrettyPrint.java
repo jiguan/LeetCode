da0e1f47cb7fd1a530c08b6970793e016fc9a548
Jianqing Guan <gjqvictory888@gmail.com>
2016-05-18 00:17:22 -0400

    216 / 332

--- src/practice/first/util/PrettyPrint.java
+++ src/practice/first/util/PrettyPrint.java
@@ -1,30 +1,37 @@
 package practice.first.util;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class PrettyPrint {
 	static public void print(ListNode node) {
-		while(node!=null) {
-			System.out.print(node.val+"->");
+		while (node != null) {
+			System.out.print(node.val + "->");
 			node = node.next;
 		}
 		System.out.println("null");
 	}
-	
+
 	static public void print(int[] arr) {
 		System.out.print("[");
-		for(int num : arr) {
-			System.out.print(num+", ");
+		for (int num : arr) {
+			System.out.print(num + ", ");
 		}
 		System.out.println("]");
 	}
-	
+
 	static public void print(List<Integer> arr) {
 		System.out.print("[");
-		for(int num : arr) {
-			System.out.print(num+", ");
+		for (int num : arr) {
+			System.out.print(num + ", ");
 		}
 		System.out.println("]");
 	}
-	
+
+	static public void print(Map map) {
+		System.out.print(Arrays.toString(map.entrySet().toArray()));
+	}
+
 }
