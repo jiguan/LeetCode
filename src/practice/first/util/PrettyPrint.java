844cde13bfbbe02e62e7034c5582addd2c3c167e
Jianqing Guan <gjqvictory888@gmail.com>
2016-05-19 00:09:44 -0400

    222 / 332

--- src/practice/first/util/PrettyPrint.java
+++ src/practice/first/util/PrettyPrint.java
@@ -5,13 +5,13 @@ import java.util.List;
 import java.util.Map;
 
 public class PrettyPrint {
-    static public void print(ListNode node) {
-        while (node != null) {
-            System.out.print(node.val + "->");
-            node = node.next;
-        }
-        System.out.println("null");
-    }
+	static public void print(ListNode node) {
+		while (node != null) {
+			System.out.print(node.val + "->");
+			node = node.next;
+		}
+		System.out.println("null");
+	}
 
 	static public void print(int[] arr) {
 		System.out.print("[");
@@ -20,27 +20,47 @@ public class PrettyPrint {
 		}
 		System.out.println("]");
 	}
-	
+
 	static public void print(String[] arr) {
-        System.out.print("[");
-        for (String str : arr) {
-            System.out.print(str + ", ");
-        }
-        System.out.println("]");
-    }
+		System.out.print("[");
+		for (String str : arr) {
+			System.out.print(str + ", ");
+		}
+		System.out.println("]");
+	}
+	
+	static public void print(char[][] chars) {
+		System.out.print("[");
+		for (int i=0;i<chars.length;i++) {
+			char[] arr = chars[i];
+			if(i==0) {
+				System.out.print("[");
+			} else {
+				System.out.print(" [");
+			}
+			for(char c : arr) {
+				System.out.print(c + " ");
+			}
+			if(i==chars.length-1) {
+				System.out.println("]]");
+			} else {
+				System.out.println("]");
+			}
+		}
 
-    static public void print(List<Integer> arr) {
-        System.out.print("[");
-        for (int num : arr) {
-            System.out.print(num + ", ");
-        }
-        System.out.println("]");
-    }
+	}
 
-    static public void print(TreeNode root) {
-        BTreePrinter.printNode(root);
-    }
+	static public <T> void print(List<T> list) {
+		System.out.print("[");
+		for (T element : list) {
+			System.out.print(element + ", ");
+		}
+		System.out.println("]");
+	}
 
+	static public void print(TreeNode root) {
+		BTreePrinter.printNode(root);
+	}
 
 	static public <K, V> void print(Map<K, V> map) {
 		System.out.print(Arrays.toString(map.entrySet().toArray()));
--- test/practice/first/array/GroupAnagrams.java
+++ test/practice/first/array/GroupAnagrams.java
@@ -1,41 +1,45 @@
 package practice.first.array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.TreeMap;
 
 import org.junit.Test;
 
-import practice.first.util.PrettyPrint;
-
 public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
-        if(strs.length==0) return res;
-        Map<Integer, PriorityQueue<String>> map = new HashMap<>();
-        for(String str : strs) {
-            map.computeIfAbsent(getCode(str), k -> new PriorityQueue<>()).add(str);
-        }
-        for(PriorityQueue<String> queue : map.values()) {
-            res.add(new ArrayList<String>(queue));
-        }
-        return res;
-    }
-    
-    private int getCode(String str) {
-        int code = 0;
-        for(char c : str.toCharArray()) {
-            code += c-'a';
-        }
-        return code;
+    	Map<String, ArrayList<String>> map = new HashMap<>();
+    	for(String str : strs) {
+    		char[] chars = str.toCharArray();
+    		Arrays.sort(chars);
+    		map.computeIfAbsent(new String(chars), k->new ArrayList<String>()).add(str);
+    	}
+    	List<List<String>> res = new ArrayList<>();
+    	for(ArrayList<String> list : map.values()) {
+    		Collections.sort(list);
+    		res.add(list);
+    	}
+    	return res;
     }
     
     @Test
     public void test0() {
         String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> result = groupAnagrams(strs);
+        for(List<String> list : result) {
+            for(String s : list) {
+                System.out.print(s+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+    @Test
+    public void test1() {
+        String[] strs = new String[]{"ac", "bb"};
         List<List<String>> result = groupAnagrams(strs);
         for(List<String> list : result) {
             for(String s : list) {
