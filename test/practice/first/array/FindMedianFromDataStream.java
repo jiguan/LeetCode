46c83fd2bce7cdafa38d9f3d702086612f8f262a
Ricky Guan <ricky.guan@cookmedical.com>
2016-06-22 16:21:44 -0400

    160 / 346 second time

--- test/practice/first/array/FindMedianFromDataStream.java
+++ test/practice/first/array/FindMedianFromDataStream.java
@@ -1,16 +1,61 @@
 package practice.first.array;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+import org.junit.Test;
+
 public class FindMedianFromDataStream {
-	int[] pool = new int[2];
-	
-	// Adds a number into the data structure.
-	public void addNum(int num) {
+    private static final double DELTA = 1e-15;
 
-	}
+    // small's order is from big to small, large is from small to big
+    PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder()), large = new PriorityQueue<>();
 
-	// Returns the median of current data stream
-	public double findMedian() {
-		return 0L;
-	}
+    // Adds a number into the data structure.
+    public void addNum(int num) {
+        large.add(num);
+        small.add(large.poll());
+        if (large.size() < small.size()) {
+            // move biggest to large queue
+            large.add(small.poll());
+        }
+    }
+
+    // Returns the median of current data stream
+    public double findMedian() {
+        if (small.size() == large.size()) {
+            return (small.peek() + large.peek()) / 2.0;
+        } else {
+            // large size is always equal or bigger than small
+            return large.peek();
+        }
+    }
+
+    @Test
+    public void test0() {
+        addNum(2);
+        addNum(3);
+        assertEquals(2.5, findMedian(), DELTA);
+    }
+
+
+    @Test
+    public void test1() {
+        addNum(2);
+        addNum(3);
+        addNum(4);
+        assertEquals(3, findMedian(), DELTA);
+    }
+
+
+    @Test
+    public void test3() {
+        addNum(-1);
+        addNum(-2);
+        addNum(-3);
+        assertEquals(-2, findMedian(), DELTA);
+    }
 
 }
