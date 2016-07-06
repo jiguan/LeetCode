f0bb7d39742f3face80e332055be4b785c76c7ff
Ricky Guan <ricky.guan@cookmedical.com>
2016-07-06 10:12:05 -0400

    180 / 354

---
+++ test/practice/first/tree/BSTToSortedLinkedList.java
@@ -0,0 +1,42 @@
+package practice.first.tree;
+
+import org.junit.Test;
+
+import practice.first.util.PrettyPrint;
+import practice.first.util.TreeNode;
+
+public class BSTToSortedLinkedList {
+
+    private TreeNode convert(TreeNode node) {
+        if (node == null || (node.left == null && node.right == null))
+            return node;
+        if (node.left != null) {
+            TreeNode min = convert(node.left);
+            TreeNode tmp = min;
+            while (tmp.right != null) {
+                tmp = tmp.right;
+            }
+            tmp.right = node;
+            node.left = null;
+            node = min;
+        }
+        if (node.right != null) {
+            node.right = convert(node.right);
+        }
+        return node;
+    }
+
+    @Test
+    public void test0() {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right = new TreeNode(5);
+        TreeNode res = convert(root);
+
+        PrettyPrint.print(res);
+
+    }
+
+}
