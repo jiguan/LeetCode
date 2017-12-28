74a5544d3a2106c616dbd6779885f9e1c671339c
Jianqing Guan <gjqvictory888@gmail.com>
2017-12-28 01:13:39 -0800

    177 / 719

--- test/com/leetcode/array/CourseSchedule.java
+++ test/com/leetcode/array/CourseSchedule.java
@@ -10,56 +10,50 @@ import org.junit.Test;
 
 public class CourseSchedule {
 	public boolean canFinish(int numCourses, int[][] prerequisites) {
-		int[][] preCurMat = new int[numCourses][numCourses];
-		int[] hasPreCourse = new int[numCourses];
-		for(int[] pair : prerequisites) {
-			int cur = pair[0], pre = pair[1];
-			if(preCurMat[pre][cur]==0) {
-				hasPreCourse[cur]++;
-			}
-			preCurMat[pre][cur] = 1;			
+		int[] indegree = new int[numCourses];
+		for (int[] pair : prerequisites) {
+			++indegree[pair[1]];
 		}
-		
+
 		Queue<Integer> unlimitedCourse = new LinkedList<>();
-		for(int i=0;i<numCourses;i++) {
-			if(hasPreCourse[i]==0) {
+		for (int i = 0; i < numCourses; i++) {
+			if (indegree[i] == 0) {
 				unlimitedCourse.offer(i);
 			}
 		}
-		int reachableCourse = 0;
-		while(!unlimitedCourse.isEmpty()) {
+		while (!unlimitedCourse.isEmpty()) {
 			int course = unlimitedCourse.poll();
-			for(int i=0;i<numCourses;i++) {
-				if(preCurMat[course][i]!=0) {
-					if(--hasPreCourse[i]==0) {
-						unlimitedCourse.offer(i);
+			--numCourses;
+			for (int[] pair : prerequisites) {
+				if (pair[0] == course) {
+					if (--indegree[pair[1]] == 0) {
+						unlimitedCourse.offer(pair[1]);
 					}
 				}
 			}
-			reachableCourse++;
 		}
-		return reachableCourse==numCourses;
+		return numCourses == 0;
 	}
-	
+
 	@Test
 	public void test0() {
 		int numCourses = 2;
-		int[][] prerequisites = new int[][]{{1,0}};
+		int[][] prerequisites = new int[][] { { 1, 0 } };
 		assertTrue(canFinish(numCourses, prerequisites));
 	}
-	
+
 	@Test
 	public void test1() {
 		int numCourses = 2;
-		int[][] prerequisites = new int[][]{{1,0}, {0,1}};
+		int[][] prerequisites = new int[][] { { 1, 0 }, { 0, 1 } };
 		assertFalse(canFinish(numCourses, prerequisites));
 	}
-	
+
 	@Test
 	public void test2() {
 		int numCourses = 3;
-		int[][] prerequisites = new int[][]{{1,0}, {0,2}, {2,1}};
+		int[][] prerequisites = new int[][] { { 1, 0 }, { 0, 2 }, { 2, 1 } };
 		assertFalse(canFinish(numCourses, prerequisites));
 	}
-	
+
 }
